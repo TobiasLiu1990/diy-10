a474c54fe619c84fc58ab5912c42c583f21fdfcf
TobiasLiu1990 <tobias.liu@live.se>
2023-10-31 09:08:13 +0100

    s

---
+++ src/main/java/com/example/devops/diy10/Diy10Application.java
@@ -0,0 +1,13 @@
+package com.example.devops.diy10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Diy10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Diy10Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devops/diy10/Diy10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops.diy10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Diy10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
